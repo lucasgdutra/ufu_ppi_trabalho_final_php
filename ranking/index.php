16f0e399cb8bf4393f216205e3465897255c9e48
Lucas Gabriel Dutra de Souza <lucas.gabriel@valecard.com.br>
2023-11-15 17:32:59 +0000

    busca dados ranking do bd

--- ranking/index.php
+++ ranking/index.php
@@ -1,3 +1,35 @@
+<?php
+// CREATE TABLE `usuarios` (
+//   `id` int(11) NOT NULL AUTO_INCREMENT,
+//   `nome` varchar(255) DEFAULT NULL,
+//   `senha` char(255) DEFAULT NULL,
+//   `pontos` int(11) DEFAULT NULL,
+//   `email` varchar(255) DEFAULT NULL,
+//   `imagem` blob DEFAULT NULL,
+//   `isAdmin` tinyint(1) DEFAULT NULL,
+//   `recorde_segundos` int(11) DEFAULT NULL,
+//   PRIMARY KEY (`id`)
+// )
+
+$usuarios_array = [];
+require_once "../classes/Database.php";
+try {
+  $database = new Database('mysql');
+  $conn = $database->getConnection();
+
+  // Modify the SQL query to order users by points and then by record time
+  $sql = "SELECT * FROM usuarios ORDER BY pontos DESC, recorde_segundos ASC";
+  $stmt = $conn->prepare($sql);
+  $stmt->execute();
+
+  $usuarios_array = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  $database->closeConnection();
+} catch (\PDOException $e) {
+  echo "Database connection failed: " . $e->getMessage();
+} catch (\InvalidArgumentException $e) {
+  echo "Invalid database dialect: " . $e->getMessage();
+}
+?>
 <?php include '../includes/navbar.php'; ?>
 <main class="container">
   <table class="table">
@@ -10,24 +42,20 @@
       </tr>
     </thead>
     <tbody>
-      <tr>
-        <th scope="row">1</th>
-        <td>Mark</td>
-        <td>40</td>
-        <td>01:26</td>
-      </tr>
-      <tr>
-        <th scope="row">2</th>
-        <td>Jacob</td>
-        <td>30</td>
-        <td>01:30</td>
-      </tr>
-      <tr>
-        <th scope="row">3</th>
-        <td>Larry</td>
-        <td>20</td>
-        <td>01:35</td>
-      </tr>
+      <?php
+      // Initialize position counter
+      $position = 1;
+
+      // Iterate through the users array to populate the table
+      foreach ($usuarios_array as $usuario) {
+        echo "<tr>";
+        echo "<th scope='row'>" . $position++ . "</th>";
+        echo "<td>" . htmlspecialchars($usuario['nome']) . "</td>";
+        echo "<td>" . $usuario['pontos'] . "</td>";
+        echo "<td>" . gmdate("i:s", $usuario['recorde_segundos']) . "</td>";
+        echo "</tr>";
+      }
+      ?>
     </tbody>
   </table>
 </main>
